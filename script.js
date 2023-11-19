b248ac42fe61d1c6cc0831fff45de61ecccde0a9
Quinton <supremecontractingva@gmail.com>
2023-11-19 14:38:14 -0500

    first push

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    // Your jQuery code here
+$('#header').prepend('<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>');
+	
+$("#menu-icon").on("click", function(){
+$("nav").slideToggle();
+$(this).toggleClass("active");
+});
+
+});
+
+let scrollPosition = window.scrollY;
+const header = document.querySelector("#header");
+const topnav = document.querySelector(".top-nav");
+const headerShrink = 1;
+
+window.addEventListener("scroll", function () {
+    scrollPosition = window.scrollY;
+    if (scrollPosition >= headerShrink) {
+        header.classList.add("nav-scrolling");
+    } else {
+        header.classList.remove("nav-scrolling");
+    }
+    if (scrollPosition >= headerShrink) {
+        topnav.classList.add("topnav-scrolling");
+    } else {
+        topnav.classList.remove("topnav-scrolling");
+    }
+});
+
+
+
+
