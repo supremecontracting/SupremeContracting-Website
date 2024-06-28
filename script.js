db62c64a0c4655ab66d31af83c4d5991679969e3
Quinton <supremecontractingva@gmail.com>
2024-06-28 16:15:28 -0400

    gutter images added to gallery

--- script.js
+++ script.js
@@ -134,5 +134,69 @@ document.addEventListener('DOMContentLoaded', function() {
     lastScrollTop = scrollTop;
   });
 
-  
+  /* lazy loading */
+document.addEventListener("DOMContentLoaded", function() {
+  function initializeLazyLoading() {
+    let lazyImages = [].slice.call(document.querySelectorAll(".tab.active img.lazy"));
+
+    if ("IntersectionObserver" in window) {
+      let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            let lazyImage = entry.target;
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.classList.remove("lazy");
+            lazyImageObserver.unobserve(lazyImage);
+          }
+        });
+      });
+
+      lazyImages.forEach(function(lazyImage) {
+        lazyImageObserver.observe(lazyImage);
+      });
+    } else {
+      // Fallback for browsers that do not support IntersectionObserver
+      let lazyLoad = function() {
+        let active = false;
+
+        if (active === false) {
+          active = true;
+
+          setTimeout(function() {
+            lazyImages.forEach(function(lazyImage) {
+              if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
+                lazyImage.src = lazyImage.dataset.src;
+                lazyImage.classList.remove("lazy");
+
+                lazyImages = lazyImages.filter(function(image) {
+                  return image !== lazyImage;
+                });
+
+                if (lazyImages.length === 0) {
+                  document.removeEventListener("scroll", lazyLoad);
+                  window.removeEventListener("resize", lazyLoad);
+                  window.removeEventListener("orientationchange", lazyLoad);
+                }
+              }
+            });
+
+            active = false;
+          }, 200);
+        }
+      };
+
+      document.addEventListener("scroll", lazyLoad);
+      window.addEventListener("resize", lazyLoad);
+      window.addEventListener("orientationchange", lazyLoad);
+    }
+  }
+
+  // Initialize lazy loading on the initially active tab
+  initializeLazyLoading();
+
+  // Reinitialize lazy loading when a new tab is clicked
+  document.querySelectorAll('.tabs-nav a').forEach((link) => {
+    link.addEventListener('click', initializeLazyLoading);
+  });
+});
 
